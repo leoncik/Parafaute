962f321262545e83014c70287f360457abc3165e
Léonard Wojcik <leonardw@tutanota.com>
2022-07-30 13:30:02 +0200

    remove: remove anglicism level selection - Keep only a single anglicism option (may be added later if needed).

--- src/popup/options.js
+++ src/popup/options.js
@@ -58,23 +58,6 @@ extensionScopeInput.addEventListener("input", () => {
   }
 })
 
-
-// Display anglicism level if anglicism is active
-const anglicismFilter = document.querySelector('#anglicismes');
-anglicismFilter.addEventListener("input", () => {
-      document.querySelector('.anglicism-options').classList.toggle('options-activated');
-})
-
-// Manage anglicism level
-const anglicismLevelInput = document.querySelector('#anglicism-level');
-const anglicismLevelOutput = document.querySelector('.anglicism-level-output');
-
-anglicismLevelOutput.textContent = anglicismLevelInput.value;
-
-anglicismLevelInput.addEventListener("input", () => {
-    anglicismLevelOutput.textContent = anglicismLevelInput.value;
-})
-
 // Button to reload current tab in order to activate changed settings
 const reloadMainTab = () => {
   reloadTabButton.classList.add('hidden');
