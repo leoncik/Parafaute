1ff188b026b361b1cb072e9d9fb1002b52832ad4
Léonard Wojcik <leonardw@tutanota.com>
2024-09-22 13:22:10 +0200

    fix: compatibility issues with Firefox

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,21 +1,21 @@
 let tabCounts = {};
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
+browser.tabs.onActivated.addListener((activeInfo) => {
   updateBadgeForTab(activeInfo.tabId);
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete") {
     updateBadgeForTab(tabId);
   }
 });
 
-chrome.tabs.onRemoved.addListener((tabId) => {
+browser.tabs.onRemoved.addListener((tabId) => {
   delete tabCounts[tabId];
 });
 
 function updateBadgeForTab(tabId) {
-  chrome.tabs.sendMessage(tabId, { action: "getCount" }, (response) => {
+  browser.tabs.sendMessage(tabId, { action: "getCount" }, (response) => {
     if (chrome.runtime.lastError) {
       updateBadge(
         {
@@ -41,18 +41,18 @@ function updateBadge(counts, tabId) {
     badgeText = totalCount > 99 ? "99+" : totalCount.toString();
   }
   
-  chrome.action.setBadgeText({
+  browser.browserAction.setBadgeText({
     text: badgeText,
     tabId: tabId,
   });
-  chrome.action.setBadgeBackgroundColor({ color: "#6B6E6F", tabId: tabId });
+  browser.browserAction.setBadgeBackgroundColor({ color: "#6B6E6F", tabId: tabId });
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "updateBadge" && sender.tab) {
     updateBadge(message.counts, sender.tab.id);
   } else if (message.action === "getDetailedCount") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs[0]) {
         sendResponse({
           counts: tabCounts[tabs[0].id] || {
