5efd23db718dc8eb585c7313c8fb8022599f0f4f
Léonard Wojcik <leonardw@tutanota.com>
2024-11-27 19:19:13 +0100

    feat: add support for 1990 reform

---
+++ src/dictionnaires/reforme1990.js
@@ -0,0 +1,29 @@
+const reforme1990 = [
+
+    //  Structure :
+    // [/faute/]: "correction",
+    
+    // VOCABULAIRE
+
+    // *** C ***
+    [/Cout\b/g, "Coût"],
+    [/cout\b/g, "coût"],
+    [/Connaitre\b/g, "Connaître"],
+    [/connaitre\b/g, "connaître"],
+    
+
+    // *** E ***
+    [/Évènement\b/g, "Événement"],
+    [/évènement\b/g, "événement"],
+
+    // *** I ***
+    [/\bIle\b/g, "Île"],
+    [/\bile\b/g, "ile"],
+
+     // *** M ***
+    [/Maître\b/g, "Maitre"],
+    [/maître\b/g, "maitre"],
+
+
+
+]
